e36ce714b6698a6dd19d9744a9e219e4101a550f
David Ernst <git@dsernst.com>
2016-11-12 13:37:16 -0800

    Download meeting agenda & move pdfs into their own folder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var path = require('path')
-var filePath = path.join(__dirname, './bag110116_minutes.pdf')
+var filePath = path.join(__dirname, 'pdfs/bag110116_agenda.pdf')
 var extract = require('pdf-text-extract')
 extract(filePath, function (err, pages) {
   if (err) {
