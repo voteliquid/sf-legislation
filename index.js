73381e726db068977a373f87b8370138bfd9e153
David Ernst <git@dsernst.com>
2017-02-07 02:32:37 -0800

    Adjust index.js to parse all downloaded agenda files

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+const util = require('util')
+const Promise = require('bluebird')
 const parseAgendaFile = require('./parse-agenda-file')
 
-parseAgendaFile('./pdfs/bag110116_agenda.pdf')
-  .then(console.log)
+const agendaDates = [
+  '011017',
+  '012417',
+  '110116',
+  '111516',
+  '112916',
+  '120616',
+  '121316',
+]
+function convertDateString(dateFormatFromFilename) {
+  // Converts '110116' => '2016-11-01'
+
+  const month = dateFormatFromFilename.slice(0, 2)
+  const day = dateFormatFromFilename.slice(2, 4)
+  const year = dateFormatFromFilename.slice(4, 6)
+
+  return `20${year}-${month}-${day}`
+}
+
+Promise.all(agendaDates)
+  .map(date => (
+    parseAgendaFile(`./pdfs/bag${date}_agenda.pdf`)
+    .map(bill => Object.assign(bill, {
+      date: convertDateString(date),
+      source_doc: `bag${date}_agenda.pdf`,
+    }))
+    .then((agenda) => {
+      console.log(util.inspect(agenda, { depth: null }))
+    })
+  ))
