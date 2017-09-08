e174c17b850d639502620c8c76ab6874816c51a6
David Ernst <git@dsernst.com>
2017-09-08 13:36:15 -0700

    Add 2017-09-12 agenda, and support for 'Urgency ordinance'

--- src/parse-agenda-pdf.js
+++ src/parse-agenda-pdf.js
@@ -34,7 +34,7 @@ function processBillText(bill) {
   // Find sponsor lines
   const startOfSponsors = /^Sponsors?: /
   if (remainingLines.some(line => startOfSponsors.test(line))) {
-    const startOfDescription = /^(Ordinance|Motion|Resolution|Hearing|Proposed|Budget and Appropriation Ordinance) /
+    const startOfDescription = /^(Ordinance|Motion|Resolution|Hearing|Proposed|Budget and Appropriation Ordinance|Urgency ordinance) /
     const sponsorLines = []
     while (!startOfDescription.test(remainingLines[0])) {
       sponsorLines.push(remainingLines.shift().trim())
