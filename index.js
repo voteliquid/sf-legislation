e9ebfcde812f107b07fbeb3f558e4c01d153e9d5
David Ernst <git@dsernst.com>
2020-02-11 12:26:09 -0800

    Add 2020-02-04 agenda

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 require('./src/download-latest-agenda.js')()
 
 // Or replace previous line w/ below to manually add a pdf
-// Promise.resolve(['102219'])
+// Promise.resolve(['020420'])
 
 // 2. Parse the pdf file into json
 .then(newPdfs => require('./src/transform-pdfs-to-json')(newPdfs))
