ecb934a2721343a208d32495045797b3217d9424
David Ernst <git@dsernst.com>
2020-03-13 12:08:36 -0700

    Add special 2020-03-19 meeting

---
+++ json/031920.js
@@ -0,0 +1,17 @@
+module.exports =
+[
+  {
+    itemNumber: 1,
+    id: 200283,
+    title: 'Hearing - Committee of the Whole - City Preparations in Response to the COVID-19 Health Emergency - March 19, 2020, at 11:00 a.m.',
+    sponsors: undefined,
+    text: 'Hearing of the Board of Supervisors sitting as a Committee of the Whole on March 19, 2020, at 11:00 a.m., for the Members of the Board of Supervisors to convene and for Departments to report regarding city preparation in response to COVID-19 health emergency; to be scheduled pending approval of the Motion contained in File No. 200282; and requesting the Mayor’s Office, Department of Emergency Management, Department of Public Health, Office of Economic and Workforce Development, Department of Human Resources, Department of Homelessness and Supportive Housing, and Department of Disability and Aging Services to report. (Clerk of the Board)',
+    fiscalImpact: false,
+    statusLog: [],
+    question: 'Question: Shall this Hearing be HEARD AND FILED?',
+    date: '2020-03-19',
+    legislature: 'san_francisco',
+    source_doc: 'bag031920_agenda.pdf',
+    uid: '2020-03-19-200283'
+  }
+]
